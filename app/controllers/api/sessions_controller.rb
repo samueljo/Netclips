f2168d637c7378762d78099db4546436f46d1c5c
Samuel Jo <samjo891@gmail.com>
2016-10-03 22:17:56 -0400

    Fill out basic user & session controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,24 @@
 class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:email],
+      params[:user][:password]
+    )
+
+    if @user.nil?
+      render json: ['Incorrect email and/or password'], status: 422
+    else
+      login!(@user)
+      render 'api/users/show'
+    end
+  end
+
+  def destroy
+    if logged_in?
+      logout!
+      render json: {}
+    else
+      render json: ['No current user to logout'], status: 404
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,43 @@
 #
 
 class User < ActiveRecord::Base
+  validates :password_digest, presence: true
+  validates :email, :session_token, presence: true, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  after_initialize :ensure_session_token
+
+  attr_reader :password
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
+  end
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64(16)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
 end
